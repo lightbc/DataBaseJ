df48fc62635a4db0560da5e30f6f58547fec9903
lightbc <lightbc@163.com>
2023-08-09 22:27:01 +0800

    1、优化SQL功能操作组的显示逻辑，非JdbcConsole编辑器时功能置为无效状态。 2、其它部分逻辑优化。

--- src/main/java/com/lightbc/databasej/actions/ContextGroup.java
+++ src/main/java/com/lightbc/databasej/actions/ContextGroup.java
@@ -1,11 +1,27 @@
 package com.lightbc.databasej.actions;
 
-import com.intellij.openapi.actionSystem.DefaultActionGroup;
+import com.intellij.database.console.JdbcConsole;
+import com.intellij.openapi.actionSystem.ActionGroup;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.lightbc.databasej.util.SqlOperateUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-public class ContextGroup extends DefaultActionGroup {
+public class ContextGroup extends ActionGroup {
 
-    public ContextGroup() {
-        super(new SaveSqlAction("Save Sql"), new UseSqlAction("Use Sql"), new DeleteSqlAction("Del Sql"));
+    @NotNull
+    @Override
+    public AnAction[] getChildren(@Nullable AnActionEvent event) {
+        JdbcConsole console = SqlOperateUtil.getConsole(event);
+        // 判断右键对象是否是JdbcConsole编辑器
+        if (console != null) {
+            SaveSqlAction saveSqlAction = new SaveSqlAction("Save Sql");
+            UseSqlAction useSqlAction = new UseSqlAction("Use Sql");
+            DeleteSqlAction deleteSqlAction = new DeleteSqlAction("Del Sql");
+            return new AnAction[]{saveSqlAction, useSqlAction, deleteSqlAction};
+        }
+        return AnAction.EMPTY_ARRAY;
     }
 
 }
